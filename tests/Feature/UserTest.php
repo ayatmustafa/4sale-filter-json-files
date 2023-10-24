b3d50543ab8575056b2c0cffffab94c8051bb228
ayatmustafa <ayatmustafa199472@gmail.com>
2023-10-24 23:55:02 +0300

    adding feature test for getting user done

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -9,10 +9,67 @@ class UserTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_get_users_from_multiple_providers_without_filtration_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('api/v1/users/testing');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertOk()
+            ->assertJsonStructure([
+                "Status",
+                "Count" ,
+                "Data" => [
+                    '*' => ['Id', 'Email', 'Currency', 'Balance', 'StatusCode', 'CreatedAt']
+                ],
+            ]);
     }
+
+    public function test_get_users_from_multiple_providers_with_statusCode_filtration_a_successful_response(): void
+    {
+        $response = $this->get('api/v1/users/testing?statusCode=authorised');
+
+        $response->assertStatus(200)
+            ->assertOk()
+            ->assertJsonStructure([
+                "Status",
+                "Count" ,
+                "Data" => [
+                    '*' => ['Id', 'Email', 'Currency', 'Balance', 'StatusCode', 'CreatedAt']
+                ],
+            ]);
+    }
+
+    public function test_get_users_from_specific_providers_with_statusCode_filtration_with_empty_a_successful_response(): void
+    {
+        $response = $this->get('api/v1/users/testing?provider=DataProviderX&statusCode=refunded');
+
+        $response->assertStatus(200)
+            ->assertOk()
+            ->assertJsonStructure([
+                "Status",
+                "Count",
+                "Data" => [
+                    '*' => ['Id', 'Email', 'Currency', 'Balance', 'StatusCode', 'CreatedAt']
+                ],
+            ])->assertJson([
+            "Status" => true,
+            "Count" => 0,
+        ]);
+    }
+
+    public function test_get_users_from_specific_providers_with_max_balance_filtration_with_a_successful_response(): void
+    {
+        $response = $this->get('api/v1/users/testing?provider=DataProviderX&balanceMax=100');
+
+        $response->assertStatus(200)
+            ->assertOk()
+            ->assertJsonStructure([
+                "Status",
+                "Count",
+                "Data" => [
+                    '*' => ['Id', 'Email', 'Currency', 'Balance', 'StatusCode', 'CreatedAt']
+                ],
+            ]);
+    }
+
 }
